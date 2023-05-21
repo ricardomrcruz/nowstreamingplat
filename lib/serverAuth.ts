a4a6077d0af3dc47465e512e22e8e66405777ddc
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2023-05-21 21:52:20 +0200

    profiles done

--- lib/serverAuth.ts
+++ lib/serverAuth.ts
@@ -3,4 +3,26 @@ import {getSession} from 'next-auth/react';
 
 import prismadb from '@/lib/prismadb';
 
-const serverAuth = async (req: NextApiRequest)
+
+const serverAuth = async (req: NextApiRequest) => {
+    const session = await getSession ({ req });
+
+    if (!session?.user?.email) {
+        throw new Error('Not Signed in');
+    }
+
+    const currentUser = await prismadb.user.findUnique({
+        where: {
+            email: session.user.email,
+        }
+    });
+
+    if(!currentUser){
+        throw new Error('Not signed in')
+    }
+
+    return { currentUser };
+
+};
+
+export default serverAuth;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,40 @@
+import { NextPageContext } from 'next';
+import { getSession, signOut} from 'next-auth/react';
+
+import useCurrentUser from '@/hooks/useCurrentUser';
+ 
+export async function getServerSideProps(context: NextPageContext) {
+
+  const session = await getSession(context);
+
+  // if session is not started aka login not done user gets redirected to login page
+
+  if(!session) {
+    return {
+      redirect: {
+        destination: '/auth', 
+        permanent: false,
+      }
+    }
+  }
+
+  return{
+    props: {}
+  }
+}
+
 export default function Home() {
+
+  const { data: user } = useCurrentUser();
+
+
+
+
   return (
     <>
-      <h1 className="text-2xl text-green-500">now - the future of streaming</h1>
+      <h1 className="text-4xl text-green-500">now - the future of streaming</h1>
+      <p className="text-white">Logged in as : {user?.email}</p>
+      <button className='h-10 w-full bg-white' onClick={() => signOut()}>Logout!</button>
     </>
   )
 }
